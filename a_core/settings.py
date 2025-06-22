d1433e9ca4537e2f9474a49924b07df9022b88d2
gauravarvindwani <wanigaurav2705@gmail.com>
2025-06-22 16:57:34 +0530

    cloudinary

--- a_core/settings.py
+++ a_core/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'django.contrib.sites',
     'admin_honeypot',
     "django_htmx",
@@ -170,7 +172,26 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if ENVIRONMENT == 'production' or POSTGRES_LOCALY == True:
+    # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES={
+        "default":{
+            "BACKEND":"cloudinary_storage.storage.MediaCloudinaryStorage",
+            
+        },
+        "staticfiles":{
+            "BACKEND":"django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
+else:
+    MEDIA_ROOT = BASE_DIR / 'media' 
+    
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('CLOUD_API_KEY'),
+    'API_SECRET': env('CLOUD_API_SECRET')
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
